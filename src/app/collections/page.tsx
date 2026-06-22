a654f39d39147d7cd4bb99c744fdf883cd471b4f
unknown <noumanmustafa@hotmail.com>
2026-06-23 00:33:52 +0500

    fixed import

--- src/app/collections/page.tsx
+++ src/app/collections/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import Link from "next/link";
 import { ArrowRight, Star, Sparkles, Shield, Clock, Gem } from "lucide-react";
-import "./collections.css";
+import "./collection.css";
 
 const collections = [
   {
